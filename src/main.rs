69af825234d333c578a22fd325e0cba28a4aa099
tmc42 <tmclean1212@gmail.com>
2023-09-06 21:56:20 -0500

    Began console menus for easy testing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 // Main file containing function for interfacing with console/gui
 // TB0x0 2023
 
-// Get parameters from the console
+// Menus and setup for using the encryption functions
 fn main() {
+    // Variables that are assigned in main
+        //source_path
+
+    // Get the parameters from the console
     let args: Vec<_> = std::env::args().collect();
+
     if args.len() > 1 {
-        println!("--TEST--\nThe first argument is {}", args[1]);
-    } else {
+        // Help screen
+        if args[1] == "-h" {
+            println!("---Usage---\nrepo256 -s <SOURCE_PATH> -d <DESTINATION_PATH> -k <KEY_FILE> -e <ENCRYPTION_METHOD>\n-----------\nCurrently supported methods: \n    None")
+        }
+        else if args[1] == "-s" {
+            // Verify path is given and if so assign variable
+            if args.len() > 2{
+                let source_path = &args[2]; // ASSIGNING source_path
+                println!("--TEST--\nThe source path is {}", source_path);
+            }
+            else {
+                println!("Error 1: Path cannot be empty");
+            }
+        }
+        else {
+            println!("--TEST--\nThe first argument is {}", args[1]);
+        }
+    } 
+    else {
         println!("No parameters given. Use -h for help.")
     }
 }
